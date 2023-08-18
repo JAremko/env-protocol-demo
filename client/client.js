278b71e545d4aaa705d6e07fcbe90073c9fb35fc
jaremko <w3techplayground@gmail.com>
2023-08-18 16:15:21 +0200

    add more js bindings

--- client/client.js
+++ client/client.js
@@ -1,18 +1,18 @@
 const protobufMessageTypes = {
-    ClientPayload: null,
-    HostPayload: null,
-    SetZoomLevel: null,
-    SetColorScheme: null,
-    SetAirTemp: null,
-    SetDistance: null,
-    SetAgcMode: null,
-    Command: null
+    ClientPayload: null, HostPayload: null, SetZoomLevel: null,
+    SetColorScheme: null, SetAirTemp: null, SetDistance: null,
+    SetAgcMode: null, Command: null, CommandResponse: null,
+    StatusOk: null, StatusError: null, SetPowderTemp: null,
+    SetAirHumidity: null, SetAirPressure: null, SetWind: null,
+    SetCompassOffset: null, SetHoldoff: null, ButtonPress: null,
+    TriggerCmd: null, SetZeroing: null, HostDevStatus: null,
+    ClientDevStatus: null
 };
 
 const protobufEnums = {
-    Zoom: null,
-    ColorScheme: null,
-    AGCMode: null
+    Zoom: null, ColorScheme: null, AGCMode: null,
+    OkStatusCode: null, ErrorStatusCode: null,
+    ButtonEnum: null, CMDDirect: null
 };
 
 let ws;
@@ -32,16 +32,76 @@ async function loadProto() {
     }
 
     getCommandMappings = () => ({
-        setZoom: data =>
-            ({ setZoom: protobufMessageTypes.SetZoomLevel.create({ zoomLevel: protobufEnums.Zoom.values[data.zoomLevel] }) }),
-        setPallette: data =>
-            ({ setPallette: protobufMessageTypes.SetColorScheme.create({ scheme: protobufEnums.ColorScheme.values[data.scheme] }) }),
-        setAirTemp: data =>
-            ({ setAirTC: protobufMessageTypes.SetAirTemp.create({ temperature: data.temperature }) }),
-        setDst: data =>
-            ({ setDst: protobufMessageTypes.SetDistance.create({ distance: data.distance }) }),
-        setAgc: data =>
-            ({ setAgc: protobufMessageTypes.SetAgcMode.create({ mode: protobufEnums.AGCMode.values[data.agcMode] })}),
+        setZoom: data => ({
+            setZoom: protobufMessageTypes.SetZoomLevel.create({
+                zoomLevel: protobufEnums.Zoom.values[data.zoomLevel]
+            })
+        }),
+        setPallette: data => ({
+            setPallette: protobufMessageTypes.SetColorScheme.create({
+                scheme: protobufEnums.ColorScheme.values[data.scheme]
+            })
+        }),
+        setAirTemp: data => ({
+            setAirTC: protobufMessageTypes.SetAirTemp.create({
+                temperature: data.temperature
+            })
+        }),
+        setDst: data => ({
+            setDst: protobufMessageTypes.SetDistance.create({
+                distance: data.distance
+            })
+        }),
+        setAgc: data => ({
+            setAgc: protobufMessageTypes.SetAgcMode.create({
+                mode: protobufEnums.AGCMode.values[data.agcMode]
+            })
+        }),
+        setPowderTemp: data => ({
+            setPowderTemp: protobufMessageTypes.SetPowderTemp.create({
+                temperature: data.temperature
+            })
+        }),
+        setAirHum: data => ({
+            setAirHum: protobufMessageTypes.SetAirHumidity.create({
+                humidity: data.humidity
+            })
+        }),
+        setAirPress: data => ({
+            setAirPress: protobufMessageTypes.SetAirPressure.create({
+                pressure: data.pressure
+            })
+        }),
+        setWind: data => ({
+            setWind: protobufMessageTypes.SetWind.create({
+                direction: data.direction, speed: data.speed
+            })
+        }),
+        setCompassOffset: data => ({
+            setMagOffset: protobufMessageTypes.SetCompassOffset.create({
+                offset: data.offset
+            })
+        }),
+        setHoldoff: data => ({
+            setHoldoff: protobufMessageTypes.SetHoldoff.create({
+                x: data.x, y: data.y
+            })
+        }),
+        buttonPress: data => ({
+            buttonPress: protobufMessageTypes.ButtonPress.create({
+                buttonPressed: protobufEnums.ButtonEnum.values[data.buttonPressed]
+            })
+        }),
+        cmdTrigger: data => ({
+            cmdTrigger: protobufMessageTypes.TriggerCmd.create({
+                cmd: protobufEnums.CMDDirect.values[data.cmd]
+            })
+        }),
+        setZeroing: data => ({
+            setZeroing: protobufMessageTypes.SetZeroing.create({
+                x: data.x, y: data.y
+            })
+        }),
         invalid: () => ({})
     });
 }
@@ -49,17 +109,17 @@ async function loadProto() {
 function setupWebSocket() {
     ws = new WebSocket('ws://localhost:8085');
     ws.binaryType = "arraybuffer";
-
     ws.onopen = event => console.log('WebSocket connection opened:', event);
     ws.onerror = event => console.error('WebSocket error:', event);
     ws.onclose = event => console.log('WebSocket connection closed:', event);
-
     ws.onmessage = event => handleServerMessage(event.data);
 }
 
 function handleServerMessage(data) {
     const hostPayload = protobufMessageTypes.HostPayload.decode(new Uint8Array(data));
-    const hostPayloadObject = protobufMessageTypes.HostPayload.toObject(hostPayload, { enums: String, defaults: true });
+    const hostPayloadObject = protobufMessageTypes.HostPayload.toObject(
+        hostPayload, { enums: String, defaults: true }
+    );
     displayServerLog(hostPayloadObject);
 }
 
@@ -68,7 +128,6 @@ function displayServerLog(data) {
     const codeBlock = document.createElement('code');
     codeBlock.className = 'json';
     codeBlock.textContent = JSON.stringify(data, null, 2);
-
     serverLogs.insertBefore(codeBlock, serverLogs.firstChild);
     hljs.highlightBlock(codeBlock);
 }
@@ -117,18 +176,14 @@ function displayCommandInputFields() {
 function sendCommandToServer(commandData) {
     const commandMappings = getCommandMappings();
     const command = commandMappings[commandData.commandType](commandData);
-
     const clientPayloadObject = { command };
-
     const errMsg = protobufMessageTypes.ClientPayload.verify(clientPayloadObject);
     if (errMsg) {
         console.error(`Validation error: ${errMsg}`);
         return;
     }
-
     const message = protobufMessageTypes.ClientPayload.create(clientPayloadObject);
     const buffer = protobufMessageTypes.ClientPayload.encode(message).finish();
-
     if (ws.readyState === WebSocket.OPEN) {
         ws.send(buffer);
         displayClientLog(message);
@@ -142,7 +197,6 @@ function displayClientLog(data) {
     const codeBlock = document.createElement('code');
     codeBlock.className = 'json';
     codeBlock.textContent = JSON.stringify(data, null, 2);
-
     clientLogs.insertBefore(codeBlock, clientLogs.firstChild);
     hljs.highlightBlock(codeBlock);
 }
@@ -156,4 +210,3 @@ async function init() {
 }
 
 init();
-
